7b9201aeafc76d91df7be3e9b5a8129fc6c58c7e
Paul Ashby <paul@primitive.co>
2023-12-08 17:29:18 +0000

    Remove unused files

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,6 @@
 import '../app/globals.css'
 import {Lato} from 'next/font/google'
-import Image from "next/legacy/image";
-import bgImage from "../app/icons/404Background.jpg";
 import style from "./404.module.css"
-import Header from "@/components/global/header/header"
-import Footer from "../components/global/footer/Footer";
-import { Metadata } from 'next'
 
 const lato = Lato({ subsets: ['latin'], weight: ["100", "300", "400", "700", "900"] })
 
@@ -16,14 +11,6 @@ export const metadata = {
 export default function alphamolly404() {
   return (
       <main  className={lato.className}>
-        <Image
-            src={bgImage}
-            objectFit='cover'
-            layout='fill'
-            objectPosition='center'
-            alt={"404"}
-        />
-        <Header/>
         <div className={style.err}>
           <div className={style.errNum}>404</div>
           <div className={style.errMess}>Nothing much of interest here so here’s a great picture by Guillaume Jaille
